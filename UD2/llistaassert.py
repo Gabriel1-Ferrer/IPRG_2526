53bebe90a25ff8d0ac345ab86fccf0c8ac2c8fd2
Gabriel <gabrielferrerescoto@gmail.com>
2025-12-01 11:58:14 +0100

    Practica final UD2

--- UD2/llistaassert.py
+++ UD2/llistaassert.py
@@ -1,8 +1,14 @@
-elements = ["poma", "pera", "taronja", "plàtan"]
-seleccio = None
+try: 
+ elements = ["poma", "pera", "taronja", "plàtan"]
+ seleccio = None
 
-pos = int(input("Introdueix una posició (0-3): "))
-
+ pos = int(input("Introdueix una posició (0-3): "))
+except AssertionError: 
+ print("Tens que introduir un número enter.")
+except AssertionError: 
+ print("El numero no existeix") 
+except ValueError: 
+ print("Tens que introduir un numero del 0 al 3.") 
 seleccio = elements[pos]
 print(f"L'element a la posició {pos} és: {seleccio}") 
 
