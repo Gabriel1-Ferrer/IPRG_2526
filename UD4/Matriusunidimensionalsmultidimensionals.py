ddeaaf8478db4a9834b8bfeea95dbf28f0b68541
Gabriel <gabrielferrerescoto@gmail.com>
2026-01-19 12:07:39 +0100

    Exercici 1 UD4

--- UD4/Matriusunidimensionalsmultidimensionals.py
+++ UD4/Matriusunidimensionalsmultidimensionals.py
@@ -1,4 +1,21 @@
 import numpy as np
 
-vector = np.array([[10, 20, 30, 40],[1,2,3,4]])
+vector = np.array([10, 20, 30, 40])
 print(vector)
+matriu2D = np.array(
+    [[1,2,3],
+     [4,5,6]]
+)
+print(matriu2D)
+
+
+matriu3D = np.array([
+    [[1,2], [3,4]],
+    [[5,6], [7,8]],
+    [[9,10], [11,12]] 
+])
+print(matriu3D)
+
+print(vector[0])
+print(matriu2D[1,1])
+print(matriu3D[1,1])
