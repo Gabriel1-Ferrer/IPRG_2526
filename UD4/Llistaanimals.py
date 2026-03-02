32b785af0b97e51cecd207995d1fc62302a1cca0
Gabriel <gabrielferrerescoto@gmail.com>
2026-03-02 12:12:45 +0100

    Exercicis llistes

---
+++ UD4/Llistaanimals.py
@@ -0,0 +1,20 @@
+llistaanimals = ["gos", "gat", "conill", "lluç", "tortuga"]
+
+print(llistaanimals)
+
+ultim = llistaanimals[4]
+
+print(ultim)
+
+primer = llistaanimals[0]
+
+print(primer)
+
+tercer = llistaanimals[2]
+
+print(tercer)
+
+llistaanimals = ["gos", "gat", "salmó", 5, "tortuga"]
+
+print(llistaanimals)
+
---
+++ UD4/Llistanotes.py
@@ -0,0 +1,7 @@
+llistanotes = [5, 7, 9, 4, 6, 3, 1, 8, 2, 10]
+
+print(llistanotes)
+
+[print(x) for x in llistanotes]
+
+print ([x for x in range(11) if x > 4])
