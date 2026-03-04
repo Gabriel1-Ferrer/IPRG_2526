19227a5e2cfd4bda604fdb3d52cde86f2c5d03c2
Gabriel <gabrielferrerescoto@gmail.com>
2026-03-04 14:02:20 +0100

    Exercicis llistes

---
+++ UD4/Llistabuida.py
@@ -0,0 +1,17 @@
+llistabuidaalumnes = []
+
+print(llistabuidaalumnes)
+
+llistabuidaalumnes.append("Ubay")
+llistabuidaalumnes.append("Mateo")
+llistabuidaalumnes.append("Leo")
+llistabuidaalumnes.append("Angel")
+llistabuidaalumnes.append("Pau")
+
+print(len(llistabuidaalumnes))
+
+i = 0
+while i < len(llistabuidaalumnes):
+    print(llistabuidaalumnes[i])
+    i = i +1
+
--- UD4/Llistanotes.py
+++ UD4/Llistanotes.py
@@ -5,3 +5,18 @@ print(llistanotes)
 [print(x) for x in llistanotes]
 
 print ([x for x in range(11) if x > 4])
+
+contador = 0
+
+
+for nota in llistanotes:
+  if nota >= 5: 
+        contador = contador + 1
+print(contador)
+
+contador = 0
+
+for nota in llistanotes:
+    if nota < 5:
+        contador += 1
+print(contador)
