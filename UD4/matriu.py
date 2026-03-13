9f3ef54c264711be305b25a72c06665c8bf25b7b
Gabriel <gabrielferrerescoto@gmail.com>
2026-03-13 13:57:18 +0100

    Practica UD4 matrius

---
+++ UD4/matriu.py
@@ -0,0 +1,25 @@
+import numpy as np
+temperatures = np.array([
+    [18, 20, 17],
+    [19, 21, 18],
+    [17, 19, 16],
+    [22, 24, 20],
+    [21, 23, 19],
+    [15, 22, 18],
+    [18, 20, 17]
+])
+
+print(temperatures)
+
+print(temperatures[4,1])
+
+print(temperatures[3,0])
+
+print(temperatures [:,2])
+
+print(temperatures [0,:])
+
+print(temperatures [0,:] [1,:])
+
+print(temperatures [2:0] [3:0] [4:0])
+
